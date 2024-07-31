5f26a31b7ddcd13544d18a53c5130a048a141e76
Yosiak-alv <Josias_romero60@outlook.com>
2024-07-31 16:50:16 -0600

    Update MainExampleExceptions.java

--- practice/src/app/exceptions/MainExampleExceptions.java
+++ practice/src/app/exceptions/MainExampleExceptions.java
@@ -1,7 +1,24 @@
 package app.exceptions;
 
+import javax.swing.*;
+
 public class MainExampleExceptions {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
 
+        String numerator = JOptionPane.showInputDialog("Enter the numerator:");
+        String denominator = JOptionPane.showInputDialog("Enter the denominator:");
+
+        try {
+            double result = calculator.divide(numerator, denominator);
+            JOptionPane.showMessageDialog(null, "Result: " + result);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } catch (ZeroDivisionExeption e2) {
+            JOptionPane.showMessageDialog(null, e2.getMessage());
+        }
+        finally {
+            JOptionPane.showMessageDialog(null, "Thank you for using our calculator!");
+        }
     }
 }
